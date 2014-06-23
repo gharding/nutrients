b46776440f437ff17a03e7c674db02b03a49692d
Glenn Harding <glenn.harding91@gmail.com>
2014-06-23 23:04:50 +0100

    Finished the initial layout file to complete the design of the Home page. Todo: Design the layout of products pages, search pages and review the appearance of static pages that are not the home page.

--- htdocs/protected/views/layouts/main.php
+++ htdocs/protected/views/layouts/main.php
@@ -1,5 +1,5 @@
-<?php /* @var $this Controller */ 
-	date_default_timezone_set('UTC'); 
+<?php /* @var $this Controller */
+	date_default_timezone_set('UTC');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -40,7 +40,7 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			      </button>
-			      <a class="navbar-brand" href="#"><?php echo Yii::app()->name; ?></a>
+			      <a class="navbar-brand" href="#"><?php echo Yii::app()->name; ?> <span class="glyphicon glyphicon-leaf"></span></a>
 			    </div>
 
 			    <!-- Collect the nav links, forms, and other content for toggling -->
@@ -73,23 +73,70 @@
 			  </div><!-- /.container-fluid -->
 		</nav>
 		<!-- /Menu -->
-		<div class="container">
+		<!-- Intro Content -->
+			<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+			    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+			  </ol>
+
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner">
+				<!-- <div class="container"> -->
+			    <div class="item active">
+			      <!-- Content -->
+				  	<img src="<?php echo Yii::app()->baseUrl; ?>/images/spaghetti.jpg" alt="" />
+			      <div class="carousel-caption">
+			        <h1>SLIDE UNO</h1>
+			      </div>
+			    </div>
+			    <div class="item">
+					<img src="<?php echo Yii::app()->baseUrl; ?>/images/real_plate.jpg" alt="" />
+					<div class="carousel-caption">
+						<!-- ... -->
+					</div>
+				</div>
+				<div class="item">
+					<img src="<?php echo Yii::app()->baseUrl; ?>/images/healthy.jpg" alt="" />
+					<div class="carousel-caption">
+						<!-- ... -->
+					</div>
+				</div>
+				<!-- </div> -->
+			  </div>
+
+			  <!-- Controls -->
+			  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+			</div>
+		<!-- /Intro Content -->
 	<!-- Content -->
+		<div class="container">
 			<?php if(isset($this->breadcrumbs)):?>
 				<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 					'links'=>$this->breadcrumbs,
 				)); ?><!-- breadcrumbs -->
 			<?php endif?>
-
 			<?php echo $content; ?>
 		</div>
 		<!-- /Content -->
 	<div class="clear"></div>
-	<hr />
-	<div class="container">
-		<div id="footer" style="text-align: center;">
-			Copyright &copy; <?php echo date('Y'); ?> by <?php echo Yii::app()->name; ?>.<br/>
-			All Rights Reserved.<br/>
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+			<div class="col-sm-4">
+					<h1><?php echo Yii::app()->name; ?> <span class="glyphicon glyphicon-leaf"></span></h1>
+					Copyright &copy; <?php echo date('Y'); ?> by <?php echo Yii::app()->name; ?>.<br/>
+					All Rights Reserved.<br/>
+			</div>
+			</div>
+
 		</div><!-- footer -->
 	</div>
 <!-- page -->
